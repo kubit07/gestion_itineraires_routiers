eabe22fe2e64ae60b4316525af2ae2a5dd71ffa4
Dave <davidnsd90@gmail.com>
2024-08-24 02:54:29 +0200

    Mise à Jour du trajet avec l'algorithme de djistra

--- lib/models/TravelTime.dart
+++ lib/models/TravelTime.dart
@@ -1,41 +1,29 @@
 import 'dart:convert';
 
 class TravelTime {
-    String departure;
-    String arrival;
-    String timeTravel;
-    String? timeScraping;
-    String? file;
+    final String departure;
+    final String arrival;
+    final String travel;
+    final String timeTravel;
 
     TravelTime({
         required this.departure,
         required this.arrival,
+        required this.travel,
         required this.timeTravel,
-        this.timeScraping,
-        this.file,
     });
 
     factory TravelTime.fromJson(Map<String, dynamic> json) => TravelTime(
         departure: json["departure"],
         arrival: json["arrival"],
+        travel: json["travel"] ?? '',
         timeTravel: json["time_travel"],
-        timeScraping: json["time_scraping"],
-        file: json["file"],
-    );
-
-    factory TravelTime.fromData(dynamic data) => TravelTime(
-        departure: data["departure"],
-        arrival: data["arrival"],
-        timeTravel: data["time_travel"],
-        timeScraping: data["time_scraping"],
-        file: data["file"],
     );
 
     Map<String, dynamic> toJson() => {
         "departure": departure,
         "arrival": arrival,
+        "travel": travel,
         "time_travel": timeTravel,
-        "time_scraping": timeScraping,
-        "file": file,
     };
 }
